172b4658abf4d946693a6c124a4af702f1d115b4
JoeSephian <joe.alex.hill@gmail.com>
2025-01-27 17:06:50 +0000

    Written first tests

--- integrationTest/createMapping.test.js
+++ integrationTest/createMapping.test.js
@@ -9,6 +9,11 @@ const __dirname = path.dirname(__filename);
 
 describe("Mapping API", () => {
     const filePath = path.resolve(__dirname, '../.env/force23');
+    let response;
+
+    beforeAll( async () => {
+        response = await request(app).post('/api/create-mapping/email/force23');
+    }, 10000)
 
     afterAll(() => {
         if (fs.existsSync(filePath)) {
@@ -16,9 +21,19 @@ describe("Mapping API", () => {
         }
     });
 
-    test("createMapping", async () => {
-        const response = await request(app).post('/api/create-mapping/email/force23');
+    test("createMapping",  () => {
         expect(response.status).toBe(201);
         expect(fs.existsSync(filePath + '/email/json/mapping.json')).toBe(true);
-    }, 10000);
+    });
+
+    test("check output", () => {
+        const data = JSON.parse(fs.readFileSync(filePath + '/email/json/mapping.json', "utf8"));
+        const keys = Object.keys(data);
+        expect(keys.length).toBe(4);
+        const backgrounds = Object.values(data.backgroundColors).map(color => color.oldBackground);
+
+        expect(backgrounds).toContain("rgb(201, 255, 247)");
+        expect(backgrounds).toContain("rgb(255, 255, 255)");
+    })
+
 })
