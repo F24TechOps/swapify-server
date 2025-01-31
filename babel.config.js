798630d425cdd746d1b90df284b40d864d0b331c
ScottNC <scottnc@hotmail.co.uk>
2025-01-31 14:41:23 +0000

    test can now mock functions

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    presets: [
+      ['@babel/preset-env', { targets: 'node 14' }],
+      '@babel/preset-typescript', // if you're using TypeScript
+    ],
+  };
+  
---
+++ test/unitTest/createMap.test.js
@@ -0,0 +1,20 @@
+import { jest } from '@jest/globals';
+
+jest.unstable_mockModule('../../src/backend/mapping.js', () => ({
+  createMapping: jest.fn(() => 'mocked result'),
+}));
+
+const { generateMapping } = await import('../../src/backend/createMap.js');
+const { createMapping } = await import('../../src/backend/mapping.js');
+
+describe("generate mapping", () => {
+    test("should call createMapping and return mocked result", async () => {
+        
+
+        const mapping = await generateMapping("templates", "force22");
+
+        console.log(mapping);
+
+        expect(createMapping).toHaveBeenCalled();
+    });
+});
