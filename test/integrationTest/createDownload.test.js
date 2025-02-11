6b76359bd8f2f60c4526b40d3e7bea87e6ae4867
F24TechOps <tech.ops@force24.co.uk>
2025-02-11 15:24:35 +0000

    edit writeFile to return promise

---
+++ test/integrationTest/createDownload.test.js
@@ -0,0 +1,15 @@
+import request from 'supertest';
+import app from '../../index.js';
+
+describe('POST /api/create-download', () => {
+  it('should return 200 on valid request', async () => {
+    const response = await request(app)
+      .post('/api/create-download')
+      .send({
+        type: 'templates', 
+        company: 'new', 
+      });
+      
+    expect(response.status).toBe(200);
+  });
+});
