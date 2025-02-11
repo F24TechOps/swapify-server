64b4fd9475c515cbb87abceac61185e4c772526e
ScottNC <scottnc@hotmail.co.uk>
2025-02-11 16:37:35 +0000

    removes deprecated files

--- src/backend/updateMicrosite.js
+++
@@ -1,8 +0,0 @@
-import { readAndRun, readFile } from "./runAll.js";
-
-const jsonData = await readFile('./.env/json/microsite.json');
-const update = JSON.parse(jsonData);
-
-const selections = {replaceId: true, flatten: true, update};
-
-await readAndRun('./src/html/microsite/base1/microsite.html', `.env/microsite.html`, selections, 'microsite');
--- src/imageformatter/circle.js
+++
@@ -1,9 +0,0 @@
-import { cropCircle } from './createCircle.js';
-
-const company = process.argv[2] ?? 'force';
-
-const inputFile = process.argv[3] ?? `./.env/${company}/email/final/images/portrait.png`;
-
-const outputFile = `./.env/${company}/email/final/images/circle.png`;
-
-cropCircle(inputFile, outputFile);
