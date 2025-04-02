f584b32ccda29e87ec66c521f3a12154cc5868d1
Esther Yekini <esther.yekini@force24.co.uk>
2025-04-02 09:54:27 +0100

    amend listen port

--- listen.js
+++ listen.js
@@ -1,6 +1,6 @@
 import app from './index.js';
 
-const PORT = process.env.NODE_ENV === "test" ? 5502 : process.env.PORT || 5501;
+const PORT = process.env.NODE_ENV === "test" ? 5502 : process.env.PORT || 5500;
 
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
